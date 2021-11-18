0cd6eaee8bab26c69e17fda38d332a06871d3f15
arahimov <artemiirakhimov@yandex.ru>
2021-11-18 14:54:18 +0300

    Updates

---
+++ precache-manifest.1d90d3b76435e27896e83c213e9202ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a35270c5ea52bd95215ed1e84d3f46",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "cc1788e84e7aff3b1215",
+    "url": "/calculator/static/css/main.8525d914.chunk.css"
+  },
+  {
+    "revision": "297b15801d0c3e1f3262",
+    "url": "/calculator/static/js/2.e7953890.chunk.js"
+  },
+  {
+    "revision": "cc1788e84e7aff3b1215",
+    "url": "/calculator/static/js/main.53fc3738.chunk.js"
+  },
+  {
+    "revision": "eb39d91f1f787cdfba39",
+    "url": "/calculator/static/js/runtime-main.8aa4bc99.js"
+  }
+]);
